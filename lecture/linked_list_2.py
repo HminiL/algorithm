b585cb63bdfb1ca7650d80c8f7ef2055477c8baf
Hyemin Lee <hmin10580@gmail.com>
2023-04-09 22:41:06 +0900

    algorithm lecture - delete node in linked list

--- lecture/linked_list_2.py
+++ lecture/linked_list_2.py
@@ -27,31 +27,30 @@ class NodeIntManagement:
         if self.head == "":
             print("There is no node to delete")
             return
-        if self.head == data:
-            ...
+        if self.head.data == data:
+            self.head = self.head.next
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    node.next = node.next.next
+                    return
+                else:
+                    node = node.next
+
 
 
 if __name__ == '__main__':
-    # linkedlist1 = NodeIntManagement(0)
-    # linkedlist1.desc()
-    # for i in range(1, 10):
-    #     linkedlist1.add(i)
-    # linkedlist1.desc()
-    node1 = Node(1)
-    node2 = Node(2)
-    node22 = Node(22)
-    node1.next = node2
-    node2.next = node22
-    print(node1.next)
-    temp = node1
-    node1 = node1.next
-    del temp
-    print(node1.data, node1.next.data)
-
-    node3 = Node(3)
-    node4 = Node(4)
-    node5 = Node(5)
-    node3.next = node4
-    node4.next = node5
-    node3 = node3.next
-    print(node3.data, node3.next.data)
+    linkedlist1 = NodeIntManagement(0)
+    for i in range(1, 10):
+        linkedlist1.add(i)
+    linkedlist1.desc()
+    linkedlist1.delete(0)
+    print("After deleting 0")
+    linkedlist1.desc()
+    linkedlist1.delete(5)
+    print("After deleting 5")
+    linkedlist1.desc()
+    linkedlist1.delete(9)
+    print("After deleting 9")
+    linkedlist1.desc()
